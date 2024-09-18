a906d92bb0e54fdb6cf7cf9da1643a1457065110
dancunrandy <dancunrandy@gmail.com>
2024-09-18 07:15:21 -0400

    format code

--- investment_manager/accounts/urls.py
+++ investment_manager/accounts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import InvestmentAccountViewSet, TransactionViewSet,AdminTransactionViewSet
+from .views import InvestmentAccountViewSet, TransactionViewSet, AdminTransactionViewSet
 
 router = DefaultRouter()
 router.register(r'accounts', InvestmentAccountViewSet, basename='account')
@@ -9,5 +9,4 @@ router.register(r'transactions', TransactionViewSet, basename='transaction')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin-transactions/', AdminTransactionViewSet.as_view({'get': 'list_user_transactions'})),
-
 ]
