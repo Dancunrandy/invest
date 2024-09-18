18b0d4e0be2f002e2be3d7f2f2c9ef3be55da08c
dancunrandy <dancunrandy@gmail.com>
2024-09-18 07:11:46 -0400

    Check for obj permission

--- investment_manager/accounts/permissions.py
+++ investment_manager/accounts/permissions.py
@@ -1,18 +1,28 @@
 from rest_framework import permissions
-from .models import AccountPermission
+from .models import AccountPermission, InvestmentAccount, Transaction
 
 class HasAccountPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        """
+        Check if the user has permission to access the object based on AccountPermission.
+        """
+        if isinstance(obj, InvestmentAccount):
+            account = obj
+        elif isinstance(obj, Transaction):
+            account = obj.account
+        else:
+            return False
+
         try:
-            account_permission = AccountPermission.objects.get(user=request.user, account=obj)
+            account_permission = AccountPermission.objects.get(user=request.user, account=account)
         except AccountPermission.DoesNotExist:
             return False
 
         if account_permission.permission == 'view':
-            return request.method in permissions.SAFE_METHODS  
+            return request.method in permissions.SAFE_METHODS
         elif account_permission.permission == 'crud':
-            return True  
+            return True
         elif account_permission.permission == 'post':
-            return request.method == 'POST'  
+            return request.method == 'POST'
 
         return False
